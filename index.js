a96aa9050ba4ece9eedb296ae960ecdb6b15be9a
suhailsinghbains <suhail.singhyo@gmail.com>
2019-03-20 21:22:11 +0530

    Added Animations to Sphere

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 $ = (queryString) => document.querySelector(queryString);
 
-$('a-sphere').setAttribute('color', 'blue');
+var hue = 0;
+const MAXDIS = -3;
+var zPos = MAXDIS;
+const MOVE = 0.1;
+var zSign = MOVE;
+
+function ChangeColor() {
+    hue = (hue + 1) % 360;
+    var color = `hsl(${hue}, 100%, 50%)`;
+    zPos = zPos + zSign;
+
+    if (zPos >= -MAXDIS) {
+        zSign = -MOVE;
+    }
+    else if (zPos <= MAXDIS) {
+        zSign = MOVE;
+    }
+
+    // console.log(zPos);
+    $('a-sphere').setAttribute('color', color);
+    $('a-sphere').setAttribute('position', `0 ${zPos} -4`);
+    requestAnimationFrame(ChangeColor);
+}
+
+requestAnimationFrame(ChangeColor);
